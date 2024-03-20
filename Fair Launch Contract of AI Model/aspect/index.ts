15bdb3602fa5c66d7a0736969e6426b3a1cf4bb9
ouzm <wjj315315@gmail.com>
2024-03-20 21:10:04 +0800

    aspect init

---
+++ Fair Launch Contract of AI Model/aspect/index.ts
@@ -0,0 +1,46 @@
+
+import {
+    allocate,
+    entryPoint,
+    execute,
+    IPostContractCallJP,
+    PostContractCallInput,
+} from "@artela/aspect-libs";
+
+/**
+ * Please describe what functionality this aspect needs to implement.
+ *
+ * About the concept of Aspect @see [join-point](https://docs.artela.network/develop/core-concepts/join-point)
+ * How to develop an Aspect  @see [Aspect Structure](https://docs.artela.network/develop/reference/aspect-lib/aspect-structure)
+ */
+class Aspect implements IPostContractCallJP {
+
+    /**
+     * isOwner is the governance account implemented by the Aspect, when any of the governance operation
+     * (including upgrade, config, destroy) is made, isOwner method will be invoked to check
+     * against the initiator's account to make sure it has the permission.
+     *
+     * @param sender address of the transaction
+     * @return true if check success, false if check fail
+     */
+    isOwner(sender: Uint8Array): bool {
+        return false;
+    }
+
+    /**
+     * postContractCall is a join-point which will be invoked after a contract call has finished.
+     *
+     * @param input input to the current join point
+     */
+    postContractCall(input: PostContractCallInput): void {
+        // Implement me...
+    }
+}
+
+// 2.register aspect Instance
+const aspect = new Aspect()
+entryPoint.setAspect(aspect)
+
+// 3.must export it
+export { execute, allocate }
+
